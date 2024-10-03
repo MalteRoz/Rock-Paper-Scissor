43459095e630acffc644bf9e9639f7be41499e46
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2024-10-03 11:39:43 +0200

    score count, round slider, timer and tooltip added

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
-
-// let amountOfRounds = prompt("Hur många rundor vill du spela: ");
-
-// if (isNaN(amountOfRounds) || amountOfRounds.trim() === "") {
-//   alert("Fel Format! Ange ett giltigt nummer.");
-//   amountOfRounds = prompt("Hur många rundor vill du spela: ");
-// } else {
-//   amountOfRounds = parseInt(amountOfRounds);
-//   alert(`Du har valt att spela ${amountOfRounds} rundor.`);
-// }
-
-// for (let i = 0; i < amountOfRounds; i++) 
-//   humanChoice = getHumanChoice(
-//     prompt("Välj mellan Sten, Sax, Påse").toLowerCase()
-//   );
-
-//   //HUMAN CODE
-//   function getHumanChoice(humanChoice) {
-//     if (
-//       humanChoice === "sten" ||
-//       humanChoice === "sax" ||
-//       humanChoice === "påse"
-//     ) {
-//       return humanChoice;
-//     } else {
-//       return getHumanChoice(
-//         prompt("Ogiltigt val! Välj mellan Sten, Sax, Påse").toLowerCase()
-//       );
-//     }
-//   }
-
 let humanChoice;
 let computerChoice;
 let userScore = 0;
 let computerScore = 0;
+let amountOfRounds;
+let roundCounter = 0;
+
+
+//Visar värdet på sliden
+let slider = document.querySelector('.slider');
+let sliderValue = document.querySelector('.value');
+
+sliderValue.textContent = slider.value;
+
+slider.addEventListener('input', (e) => {
+  let roundSliderValue = e.target.value;
+  sliderValue.textContent = roundSliderValue;
+
+});
+
+// Startknapp - Hämta antal rundor
+const startBtn = document.querySelector('.start')
+const lockedScreen = document.querySelector('.locked-container')
+const tooltiptext = document.querySelector('.tooltiptext')
+const tooltip = document.querySelector('.tooltip')
+
+startBtn.addEventListener('click', () => {
+  amountOfRounds = Number(slider.value);
+  console.log(`Antal rundor: ${amountOfRounds}`);
+  lockedScreen.classList.remove('lockedGame')
+  tooltiptext.textContent = '';
+  tooltip.classList.remove('tooltip')
+  //enablePlayBoard()
+  //opacity osv 
+})
 
 
 //COMPUTER CODE
@@ -47,16 +47,16 @@ function getComputerChoice() {
   }
 }
 
-function displayComputerChoice (computerChoice) {
-    if (computerChoice === 'rock' ) {
-      return computerDisplay.src = "images/rock.png";
-    }
-    else if (computerChoice === 'paper') {
-      return  computerDisplay.src = "images/paper.png";
-    }
-    else if (computerChoice === 'scissor') {
-      return  computerDisplay.src = "images/scissors.png";
-    }
+function displayComputerChoice(computerChoice) {
+  if (computerChoice === 'rock') {
+    return computerDisplay.src = "images/rock.png";
+  }
+  else if (computerChoice === 'paper') {
+    return computerDisplay.src = "images/paper.png";
+  }
+  else if (computerChoice === 'scissor') {
+    return computerDisplay.src = "images/scissors.png";
+  }
 }
 computerChoice = getComputerChoice();
 
@@ -75,84 +75,134 @@ let computerDisplay = document.querySelector('.right')
 
 rock.addEventListener('click', () => {
   humanChoice = 'rock';
+
+  startCountdown(3, () => {
+    playRound(humanChoice, computerChoice);
+    humanDisplay.src = "images/rock.png";
+  });
   
-  humanDisplay.src = "images/rock.png";
-
-
-  // elementsR.forEach(element => {
-  //   element.style.opacity = 1;
-  // });
-
-  playRound(humanChoice, computerChoice);
 })
 
 paper.addEventListener('click', () => {
   humanChoice = 'paper';
 
-  humanDisplay.src = "images/paper.png";
+  startCountdown(3, () => {
+    playRound(humanChoice, computerChoice);
+    humanDisplay.src = "images/paper.png";
+  });
 
-  playRound(humanChoice, computerChoice);
 })
 
 scissor.addEventListener('click', () => {
   humanChoice = 'scissor';
 
-  humanDisplay.src = "images/scissors.png";
 
-  playRound(humanChoice, computerChoice);
+  startCountdown(3, () => {
+    playRound(humanChoice, computerChoice);
+    humanDisplay.src = "images/scissors.png";
+  });
 })
 
 //PLAY ROUND
-
+let playerStats = document.querySelector('.playerStats')
+let compStats = document.querySelector('.compStats')
 let startTag = document.querySelector('.start-tag')
+let roundPara = document.querySelector('.roundPara')
 
 function playRound(humanChoice) {
-  computerChoice = getComputerChoice(); 
-  console.log(`Human: ${humanChoice}, Computer: ${computerChoice}`);
 
-  displayComputerChoice(computerChoice); // Skicka in datorns val
+  if (roundCounter < amountOfRounds) {
+    computerChoice = getComputerChoice();
+    console.log(`Human: ${humanChoice}, Computer: ${computerChoice}`);
 
-  if (humanChoice === computerChoice) {
-    startTag.textContent = 'DRAW!';
-  } else if (humanChoice === "rock" && computerChoice === "paper") {
-    startTag.textContent = 'YOU LOSE! PAPER BEATS ROCK!';
-    computerScore += 1;
-  } else if (humanChoice === "rock" && computerChoice === "scissor") {
-    startTag.textContent = 'YOU WIN! ROCK BEATS SCISSOR!';
-    userScore += 1;
-  } else if (humanChoice === "scissor" && computerChoice === "rock") {
-    startTag.textContent = 'YOU LOSE! ROCK BEATS SCISSOR!';
-    computerScore += 1;
-  } else if (humanChoice === "scissor" && computerChoice === "paper") {
-    startTag.textContent = 'YOU WIN! SCISSOR BEATS PAPER!';
-    userScore += 1;
-  } else if (humanChoice === "paper" && computerChoice === "scissor") {
-    startTag.textContent = 'YOU LOSE! SCISSOR BEATS PAPER!';
-    computerScore += 1;
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    startTag.textContent = 'YOU WIN! PAPER BEATS ROCK!';
-    userScore += 1;
+    displayComputerChoice(computerChoice); 
+
+    if (humanChoice === computerChoice) {
+      startTag.textContent = 'DRAW!';
+      
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+      startTag.textContent = 'YOU LOSE! PAPER BEATS ROCK!'; 
+      computerScore += 1;
+      compStats.textContent = `Computer: ${computerScore}`
+    } else if (humanChoice === "rock" && computerChoice === "scissor") {
+      startTag.textContent = 'YOU WIN! ROCK BEATS SCISSOR!';
+      userScore += 1;
+      playerStats.textContent = `Player: ${userScore} `
+    } else if (humanChoice === "scissor" && computerChoice === "rock") {
+      startTag.textContent = 'YOU LOSE! ROCK BEATS SCISSOR!';
+      computerScore += 1;
+      compStats.textContent = `Computer: ${computerScore}`
+    } else if (humanChoice === "scissor" && computerChoice === "paper") {
+      startTag.textContent = 'YOU WIN! SCISSOR BEATS PAPER!';
+      userScore += 1;
+      playerStats.textContent = `Player: ${userScore} `
+    } else if (humanChoice === "paper" && computerChoice === "scissor") {
+      startTag.textContent = 'YOU LOSE! SCISSOR BEATS PAPER!';
+      computerScore += 1;
+      compStats.textContent = `Computer: ${computerScore}`
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      startTag.textContent = 'YOU WIN! PAPER BEATS ROCK!';
+      userScore += 1;
+      playerStats.textContent = `Player: ${userScore} `
+    }
+
+    roundCounter++;
+    roundPara.textContent = `Round: ${roundCounter}`
+    if (roundCounter >= amountOfRounds) {
+      setTimeout(() => {
+        announceWinner();  
+      }, 250);  
+    }
   }
 }
 
+function announceWinner() {
+  if (userScore > computerScore) {
+    alert(`You won the match! Final score: ${userScore} - ${computerScore}`);
+  } else if (userScore < computerScore) {
+    alert(`The computer won the match! Final score: ${userScore} - ${computerScore}`);
+  } else {
+    alert(`It's a draw! Final score: ${userScore} - ${computerScore}`);
+  }
+
+  
+  
+  //Score & Roundcounter
+  roundCounter = 0;
+  userScore = 0;
+  computerScore = 0;
+
+  //Text back to default dislay 
+  startTag.textContent = 'Lets Play!';
+  roundPara.textContent = 'Round: '
+  playerStats.textContent= 'Player: '
+  compStats.textContent= 'Computer: '
+
+  //IMG and Div back to default dislay 
+  lockedScreen.classList.add('lockedGame')
+  computerDisplay.src = "images/rock.png"
+  humanDisplay.src = "images/rock.png";
+
+  //toolTip
+  tooltiptext.textContent = 'Choose Amount of Rounds';
+  tooltip.classList.add('tooltip')
+}
 
 
-console.log("Du valde: " + humanChoice);
-console.log("Datorn valde: " + computerChoice);
-console.log("Användare: " + userScore);
-console.log("Dator: " + computerScore);
+function startCountdown(seconds, callback) {
+  let counter = seconds;
+
+  const interval = setInterval(() => {
+    startTag.textContent = counter;
+    console.log(counter);
+    if (counter <= 0 ) {
+      clearInterval(interval);
+      if (callback) callback();
+    }
+    counter--;
+  }, 750);
+}
 
 
-// let winner =
-//   userScore > computerScore
-//     ? "Du vann matchen! Slutresultat blev: " + computerScore + " Användare: " + userScore
-//     : userScore === computerScore
-//       ? "Det blev lika. Slutresultat blev: " + computerScore + " Användare: " + userScore
-//       : "Du förlorade matchen, datorn vann. Slutresultat blev: Dator: " +
-//       computerScore +
-//       " Användare: " +
-//       userScore;
-
-alert(winner);
 
 
